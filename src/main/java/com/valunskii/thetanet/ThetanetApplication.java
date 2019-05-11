b2f2b299c5144044f7c3a45f10799669d8bddb88
Vadim Valunskii <dvvv09@gmail.com>
2019-05-11 16:23:25 +0300

    Initial commit

---
+++ src/main/java/com/valunskii/thetanet/ThetanetApplication.java
@@ -0,0 +1,13 @@
+package com.valunskii.thetanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThetanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThetanetApplication.class, args);
+	}
+
+}
